f8672a74ad33e94489bffd849f80699a66867f48
Ashley Klein <ashklein6@gmail.com>
2019-01-10 23:01:41 -0600

    added ability to accept or decline chill requests

--- server/routes/chills.router.js
+++ server/routes/chills.router.js
@@ -110,9 +110,10 @@ router.get('/pending/:id', rejectUnauthenticated, (req, res) => {
     console.log(`in chills.router.js GET for '/chills/pending' req.params:`,req.params);
     let id=req.params.id;
     pool.query(`
-    SELECT requests.id AS request_id, chills_users_id, requester_id, created_user_id, requested_user_id, chill_id, connection_id, start_time, end_time, details FROM requests
+    SELECT requests.id AS request_id, chills_users_id, requester_id, created_user_id, requested_user_id, chill_id, connection_id, start_time, end_time, details, person.username AS friend_username FROM requests
     JOIN chills_users ON chills_users.id = requests.chills_users_id
     JOIN chills ON chills.id = chills_users.chill_id
+    JOIN person ON person.id = requests.requester_id
     WHERE (created_user_id=$1) AND (requested_user_id IS NULL);
     `, [id])
         .then((result) => {
@@ -123,27 +124,27 @@ router.get('/pending/:id', rejectUnauthenticated, (req, res) => {
     });
 });
 
-// get user's upcoming 3 scheduled chills
+// get user's upcoming 5 scheduled chills
 router.get('/upcoming/:id', rejectUnauthenticated, (req, res) => {
     console.log(`in chills.router.js GET for '/chills/upcoming' req.params:`,req.params);
     let id=req.params.id;
     pool.query(`
     SELECT connections.username AS friend_username, connections.id AS friend_id, chills_users.id AS chills_users_id, created_user_id, requested_user_id, chill_id, chills_users.connection_id, start_time, end_time, details FROM (
-        SELECT connections.id as connection_id, to_user.username, to_user.id FROM connections 
-        LEFT JOIN person AS from_user ON from_user.id=from_user_id
-        LEFT JOIN person AS to_user ON to_user.id=to_user_id
-        WHERE (from_user.id=$1) AND (accepted = true)
-        UNION ALL
-        SELECT connections.id as connection_id, from_user.username, from_user.id FROM connections 
-        LEFT JOIN person AS from_user ON from_user.id=from_user_id
-        LEFT JOIN person AS to_user ON to_user.id=to_user_id
-        WHERE (to_user_id=$1) AND (accepted = true)
+        SELECT connections.id AS connection_id, to_user.username, to_user.id FROM connections
+        LEFT JOIN person AS from_user ON from_user.id=from_user_id
+        LEFT JOIN person AS to_user ON to_user.id=to_user_id
+        WHERE (from_user.id=$1) AND (accepted = true)
+        UNION ALL
+        SELECT connections.id AS connection_id, from_user.username, from_user.id FROM connections
+        LEFT JOIN person AS from_user ON from_user.id=from_user_id
+        LEFT JOIN person AS to_user ON to_user.id=to_user_id
+        WHERE (to_user_id=$1) AND (accepted = true)
     ) AS connections
     JOIN chills_users ON chills_users.connection_id=connections.connection_id
     JOIN chills ON chills_users.chill_id=chills.id
     WHERE chills_users.requested_user_id IS NOT NULL
     ORDER BY start_time ASC
-    LIMIT 3;
+    LIMIT 5;
     `,[id])
         .then((result) => {
             res.send(result.rows);
@@ -262,19 +263,20 @@ router.put('/request/accept', rejectUnauthenticated, async (req, res) => {
     console.log(`in chills.router.js PUT for '/chills/request/accept' req.body:`,req.body);
     let userId = req.body.id;
     let friendId = req.body.friendId;
-    let chillId = req.body.chillId;
+    let chillsUsersId = req.body.chillsUsersId;
     let requestId = req.body.requestId;
-    console.log('userId:', userId, 'friendId:', friendId, 'chillId:', chillId, 'requestId:', requestId);
+    console.log('userId:', userId, 'friendId:', friendId, 'chillsUsersId:', chillsUsersId, 'requestId:', requestId);
 
     const client = await pool.connect();
 
     try {
         await client.query(`BEGIN;`);
-        let connectionId = await client.query(`SELECT * FROM connections 
+        let result = await client.query(`SELECT * FROM connections 
         WHERE (from_user_id=$1 AND to_user_id=$2) OR (from_user_id=$2 AND to_user_id=$1) AND (accepted=true);
         `,[userId, friendId]);
+        let connectionId = result.rows[0].id
         await client.query(`UPDATE chills_users SET requested_user_id=$1, connection_id=$2 WHERE id=$3;
-        `, [id, connectionId, chillId]);
+        `, [userId, connectionId, chillsUsersId]);
         await client.query(`DELETE FROM requests where id=$1`, [requestId]);
         await client.query(`COMMIT;`);
     } catch (error) {
@@ -283,6 +285,7 @@ router.put('/request/accept', rejectUnauthenticated, async (req, res) => {
         return res.status(500).send(error);
     } finally {
         client.release();
+        console.log('accept chill request was successful');
         return res.sendStatus(200)
     }
 });
@@ -296,6 +299,7 @@ router.delete('/request/decline/:id', rejectUnauthenticated, (req, res) => {
     `, [requestId])
         .then((result) => {
             console.log(result);
+            console.log('decline chill request was successful');
             res.sendStatus(200);
         }).catch((error) => {
             console.log('Error DELETE /chills', error);
